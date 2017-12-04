6261fab1f230d4bf6b9e1963d401f496b9577a4d
jsteele13 <jenni1317@gmail.com>
2017-12-04 14:48:04 -0600

    Started - 12/4/17

--- src/Tile.java
+++ src/Tile.java
@@ -2,4 +2,13 @@ import javax.swing.JButton;
 
 public class Tile {
 	private JButton button;
+	
+	private int currentX = 0;
+	private int currentY = 0;
+	private int completedX = 0;
+	private int completedY = 0;
+	
+	
+	
+	
 }
