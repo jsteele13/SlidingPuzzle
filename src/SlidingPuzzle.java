41af04c7222f6e8dd51c6080406f41d09d2a53c4
MAC <MAC@wirelessprv-10-195-3-152.near.illinois.edu>
2017-12-05 16:09:31 -0600

    JPanel and GUI

--- src/SlidingPuzzle.java
+++ src/SlidingPuzzle.java
@@ -1,15 +1,59 @@
+ import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+ 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-public class SlidingPuzzle {
+
+public class SlidingPuzzle extends JPanel implements ActionListener{
 	private Tile[][] board;
 	
 	private boolean isFinished;
 	
 	public SlidingPuzzle() {
 		board = new Tile[3][3];
+		for (int i = 0; i < board.length; i++) {
+			
+		}
 		isFinished = false;
 	}
-	
-	public void main(String[] args) {
-		;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
+	
+    /**
+     * Create the GUI and show it.  For thread safety, 
+     * this method should be invoked from the 
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+ 
+        //Create and set up the window.
+        JFrame frame = new JFrame("SlidingPuzzle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Create and set up the content pane.
+        SlidingPuzzle newContentPane = new SlidingPuzzle();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI(); 
+            }
+        });
+    }
 }
