041ba3e323a16a09f304779c38ffb07a671a0910
MAC <MAC@wirelessprv-10-195-3-152.near.illinois.edu>
2017-12-06 14:35:01 -0600

    Add ActionCommands and Listener for buttons in array.

--- src/SlidingPuzzle.java
+++ src/SlidingPuzzle.java
@@ -21,8 +21,9 @@ public class SlidingPuzzle extends JPanel implements ActionListener{
 		for (int i = 0; i < board.length; i++) {
 			for(int j = 0; j  <board[0].length; j++) {
 				board[i][j] = new JButton("" + count);
+				board[i][j].setActionCommand("move" + count);
+				board[i][j].addActionListener(this);
 				count++;
-				
 			}
 		}
 		isFinished = false;
@@ -30,8 +31,44 @@ public class SlidingPuzzle extends JPanel implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < board.length; i++) {
+			for(int j = 0; j  <board[0].length; j++) {
+				if (board[i][j].getText().equals(e.getActionCommand().substring(4))) {
+					if (i == 0) {
+						if (board[i + 1][j].getText().equals("")) {
+							JButton temp = board[i + 1][j];
+							board[i + 1][j] = board[i][j];
+							board[i][j] = temp;
+							return;
+						}
+					}
+					if (i == board.length - 1) {
+						if (board[i - 1][j].getText().equals("")) {
+							JButton temp = board[i - 1][j];
+							board[i - 1][j] = board[i][j];
+							board[i][j] = temp;
+							return;
+						}
+					}
+					if (j == 0) {
+						if (board[i][j + 1].getText().equals("")) {
+							JButton temp = board[i][j + 1];
+							board[i][j + 1] = board[i][j];
+							board[i][j] = temp;
+							return;
+						}
+					}
+					if (j == board[0].length - 1) {
+						if (board[i][j - 1].getText().equals("")) {
+							JButton temp = board[i][j - 1];
+							board[i][j - 1] = board[i][j];
+							board[i][j] = temp;
+							return;
+						}
+					}
+				}
+			}
+		}
 	}
 	
 	public boolean checkFinished() {
