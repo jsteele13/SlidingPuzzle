e8f62c6706aa35b691140ce41d6572d11f40b6d0
jsteele13 <jenni1317@gmail.com>
2017-12-07 18:14:52 -0600

    arrangeButtons works

--- src/SlidingPuzzle.java
+++ src/SlidingPuzzle.java
@@ -1,27 +1,28 @@
- import javax.swing.AbstractButton;
+import javax.swing.AbstractButton;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
- 
+
+import java.awt.Container;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
-
-public class SlidingPuzzle extends JPanel implements ActionListener{
+public class SlidingPuzzle extends JPanel implements ActionListener {
 	private JButton[][] board;
 	private int size = 3;
-	private int emptyRow; //row coordinate of empty tile
-	private int emptyCol; //col coordinate of empty tile
+	private int emptyRow; // row coordinate of empty tile
+	private int emptyCol; // col coordinate of empty tile
 	private boolean isFinished;
-	
+
 	public SlidingPuzzle() {
 		board = new JButton[size][size];
 		int count = 1;
 		for (int i = 0; i < board.length; i++) {
-			for(int j = 0; j  <board[0].length; j++) {
+			for (int j = 0; j < board[0].length; j++) {
 				board[i][j] = new JButton("" + count);
 				board[i][j].setActionCommand("move" + count);
 				board[i][j].addActionListener(this);
@@ -34,7 +35,7 @@ public class SlidingPuzzle extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		for (int i = 0; i < board.length; i++) {
-			for(int j = 0; j  <board[0].length; j++) {
+			for (int j = 0; j < board[0].length; j++) {
 				if (board[i][j].getText().equals(e.getActionCommand().substring(4))) {
 					if (i == 0) {
 						if (board[i + 1][j].getText().equals("")) {
@@ -72,7 +73,7 @@ public class SlidingPuzzle extends JPanel implements ActionListener{
 			}
 		}
 	}
-	
+
 	public boolean checkFinished() {
 		int count = 1;
 		for (int i = 0; i < size; i++) {
@@ -85,44 +86,46 @@ public class SlidingPuzzle extends JPanel implements ActionListener{
 		}
 		return true;
 	}
-    /**
-     * Create the GUI and show it.  For thread safety, 
-     * this method should be invoked from the 
-     * event-dispatching thread.
-     */
-    private static void createAndShowGUI() {
- 
-        //Create and set up the window.
-        JFrame frame = new JFrame("SlidingPuzzle");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(100, 100);
-        //Create and set up the content pane.
-        SlidingPuzzle newContentPane = new SlidingPuzzle();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
- 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-        
-//        //TEST
-//        JPanel panel = new JPanel();
-//        GroupLayout layout = new GroupLayout(panel);
-//        
-//        layout.setHorizontalGroup(
-//        		layout.createSequentialGroup(
-//        				.addComponent().board[0][0])
-//        				.addcomponent(board[0][1])
-//        						)));
-        
-        
-    }
-    
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI(); 
-            }
-        });
-    }
+
+	public void arrangeButtons(Container pane) {
+
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new GridLayout(size, size));
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				buttons.add(board[i][j]);
+			}
+		}
+		pane.add(buttons);
+	}
+
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be invoked
+	 * from the event-dispatching thread.
+	 */
+	private static void createAndShowGUI() {
+
+		// Create and set up the window.
+		JFrame frame = new JFrame("SlidingPuzzle");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(100, 100);
+		// Create and set up the content pane.
+		SlidingPuzzle puzzle = new SlidingPuzzle();
+		puzzle.setOpaque(true); // content panes must be opaque
+		frame.setContentPane(puzzle);
+		puzzle.arrangeButtons(frame.getContentPane());
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
 }
