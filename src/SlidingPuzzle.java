ea083a9e50966f778a0dbe998360c435f33db8e6
MAC <MAC@minh-nguyen-3>
2017-12-10 23:14:10 -0600

    The game now works properly. Implemented swap() and fix a number of bugs. Added a pop-up message when the game is won. There is a major bug, where the buttons' initial random arrangement makes the game impossible to win.

--- src/SlidingPuzzle.java
+++ src/SlidingPuzzle.java
@@ -1,21 +1,23 @@
-import javax.swing.AbstractButton;
-import javax.swing.GroupLayout;
+//import javax.swing.AbstractButton;
+//import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
-import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+//import javax.swing.ImageIcon;
 
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+//import java.awt.event.KeyEvent;
 import java.util.HashMap;
 //NEXT STEPS:
 // 1) UI update to allow buttons to switch
 // 2) know when it wins!
 // 3) make pretty/larger
 // 4) rework for a picture
+@SuppressWarnings("serial")
 public class SlidingPuzzle extends JPanel implements ActionListener {
 	private JButton[][] board;
 	private int size = 3;
@@ -24,6 +26,8 @@ public class SlidingPuzzle extends JPanel implements ActionListener {
 	private boolean isFinished;
 	private Container pane;
 
+	
+	
 	public SlidingPuzzle(Container p) {
 		board = new JButton[size][size]; 
 		HashMap<Integer, Boolean> buttonsMade = new HashMap<Integer, Boolean>(); //Hashmap of buttons already made
@@ -36,7 +40,7 @@ public class SlidingPuzzle extends JPanel implements ActionListener {
 				do { 
 					random = (int)(Math.random() * 9) + 1;
 				} while (buttonsMade.containsKey(random));
-				//makes blank button (non-existepnt button #9)
+				//makes blank button (non-existent button #9)
 				if(random == 9) { 
 					board[i][j] = new JButton("");
 					board[i][j].setEnabled(false);
@@ -56,58 +60,47 @@ public class SlidingPuzzle extends JPanel implements ActionListener {
 		pane = p;
 	}
 
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println(e.getActionCommand().substring(4));
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
 				if (board[i][j].getText().equals(e.getActionCommand().substring(4))) {
-					if(Math.abs(i - emptyCol) == 1 ^ Math.abs(j - emptyRow) == 1) {
-						JButton temp = board[i][j];
-						board[i][j] = board[emptyRow][emptyCol];
-						board[emptyRow][emptyCol] = temp;
-						return;	
+					if((Math.abs(i - emptyRow) == 0 && Math.abs(j - emptyCol) == 1)
+							^ (Math.abs(i - emptyRow) == 1 && Math.abs(j - emptyCol) == 0)){
+						swap(board[i][j], board[emptyRow][emptyCol]);
+						emptyRow = i;
+						emptyCol = j;
+						if (checkFinished()) {
+							infoBox("YOU WIN!", "Result");
+						}
+						return;
 					}
 				}
 			}
 		}
-		//arrangeButtons(pane);
 	}
 
-						
-//					if (i == 0) {
-//						if (board[i + 1][j].getText().equals("")) {
-//							JButton temp = board[i + 1][j];
-//							board[i + 1][j] = board[i][j];
-//							board[i][j] = temp;
-//							return;
-//						}
-//					}
-//					if (i == board.length - 1) {
-//						if (board[i - 1][j].getText().equals("")) {
-//							JButton temp = board[i - 1][j];
-//							board[i - 1][j] = board[i][j];
-//							board[i][j] = temp;
-//							return;
-//						}
-//					}
-//					if (j == 0) {
-//						if (board[i][j + 1].getText().equals("")) {
-//							JButton temp = board[i][j + 1];
-//							board[i][j + 1] = board[i][j];
-//							board[i][j] = temp;
-//							return;
-//						}
-//					}
-//					if (j == board[0].length - 1) {
-//						if (board[i][j - 1].getText().equals("")) {
-//							JButton temp = board[i][j - 1];
-//							board[i][j - 1] = board[i][j];
-//							board[i][j] = temp;
-//							return;
-//						}
-//					}
+	
+	
+	public void swap(JButton one, JButton two) {
+		String oldText = new String(one.getText());
+		String newText = new String(two.getText());
+		
+		one.setText(newText);
+		one.setActionCommand("move" + newText);
+		
+		two.setText(oldText);
+		two.setActionCommand("move" + oldText);
+		
+		one.setEnabled(false);
+		two.setEnabled(true);
+	}
 
+	
+	
 	public boolean checkFinished() {
 		int count = 1;
 		for (int i = 0; i < size; i++) {
@@ -116,13 +109,25 @@ public class SlidingPuzzle extends JPanel implements ActionListener {
 					return false;
 				}
 				count++;
+				if (count == 9) {
+					break;
+				}
 			}
 		}
 		return true;
 	}
 
-	public void arrangeButtons(Container pane) {
-		pane.removeAll();
+	
+	// Display a message box
+    public static void infoBox(String infoMessage, String titleBar)
+    {
+        JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+	
+	
+	public void arrangeButtons(Container panel) {
+		panel.removeAll();
 		JPanel buttons = new JPanel();
 		buttons.setLayout(new GridLayout(size, size));
 		for (int i = 0; i < board.length; i++) {
@@ -130,10 +135,11 @@ public class SlidingPuzzle extends JPanel implements ActionListener {
 				buttons.add(board[i][j]);
 			}
 		}
-		pane.add(buttons);
-		//updateUI();
+		panel.add(buttons);
 	}
 
+	
+	
 	/**
 	 * Create the GUI and show it. For thread safety, this method should be invoked
 	 * from the event-dispatching thread.
@@ -157,6 +163,7 @@ public class SlidingPuzzle extends JPanel implements ActionListener {
 	}
 	
 
+	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
